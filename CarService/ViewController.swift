fff5ff3a344dcbf55887b97fe0b9efddb7184362
Максим Балахонов <gmoaokgsliem@gmail.com>
2018-07-19 15:37:07 +0300

    Initial Commit

---
+++ CarService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarService
+//
+//  Created by Максим Белугин on 19.07.2018.
+//  Copyright © 2018 Максим Белугин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
